f2982d941cc28b0a7cdd4d8c6a84a6045573f388
bigreni <bigreni@gmail.com>
2015-02-27 15:57:54 -0800

    Test

---
+++ Proverbs/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Sai\ProverbsAds\Proverbs\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\Sai\ProverbsAds\Proverbs\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ Proverbs/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Sai\ProverbsAds\Proverbs\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Sai\ProverbsAds\Proverbs\platforms\android\AndroidManifest.xml \
