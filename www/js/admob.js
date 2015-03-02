7b039fded25b66fe57699776cd08a680e0a14075
bigreni <bigreni@gmail.com>
2015-03-02 14:27:36 -0800

    Refactoring

--- www/js/admob.js
+++ www/js/admob.js
@@ -8,8 +8,8 @@
     var admobid = {};
     if (/(android)/i.test(navigator.userAgent)) {
         admobid = { // for Android
-            banner: 'ca-app-pub-9249695405712287/3154959152',
-            interstitial: 'ca-app-pub-9249695405712287/3301233156'
+            banner: 'ca-app-pub-3886850395157773/3411786244'
+            //interstitial: 'ca-app-pub-9249695405712287/3301233156'
         };
     }
 
